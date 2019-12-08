9faef2b5e8061e1f7a2ecd997e77ea9777f4afd4
Prishita Ray <30746562+PRISHIta123@users.noreply.github.com>
2019-12-08 23:40:41 +0530

    Added SOM code with map plots

--- src/main/java/asog/ProcessingRelay.java
+++ src/main/java/asog/ProcessingRelay.java
@@ -1,6 +1,10 @@
 package asog;
 
 import com.sun.tools.javac.util.Log;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
 import org.deeplearning4j.clustering.algorithm.Distance;
 import org.deeplearning4j.clustering.kmeans.KMeansClustering;
@@ -53,7 +57,7 @@ import java.util.Map;
  *  @version 1.0
  *
  */
-class ProcessingRelay {
+class ProcessingRelay extends JFrame{
 
 
     /**
@@ -150,13 +154,109 @@ class ProcessingRelay {
     void extractionPipeline(){
 
     }
+    
+    //Code to plot SOMs after training
+    public void plot(int xDim, int yDim, int [] nodes)
+	 {
+		 // Determine the number of observations assigned to each node
+		 int [] counts = new int[xDim * yDim];
+		 for(int i = 0; i < nodes.length; i++)
+		 {
+			 counts[nodes[i]] += 1;
+		 }
+		 // Determine the maximum and minimum counts for shading
+		 int maxCount = 0;
+		 int minCount = Integer.MAX_VALUE;
+		 for(int i = 0; i < counts.length; i++)
+		 {
+			if(counts[i] < minCount)
+			{
+				minCount = counts[i];
+			}
+			if(counts[i] > maxCount)
+			{
+				maxCount = counts[i];
+			}
+			//System.out.println("Node " + i + ": " + counts[i]);
+		 }
+		 // Create the window
+		 JFrame map = new JFrame ("Kohonen network");
+		 // Set the output resolution, don't let it exceed 1024x768
+		 //double aspectRatio = (double)xdim / yDim;
+		 String col = colorBox.getSelectedItem().toString();
+		 map.setSize(800, 600);
+		 map.setLayout (new GridLayout(yDim, xDim));
+		 
+		 // Plot the counts
+		 JButton [] jB = new JButton[counts.length];
+		 // Set the colors
+		 float r = 0;
+		 float g = 0;
+		 float b = 0;
+		 // Fill top to bottom, left to right
+		 // instead of left to right, top to bttom
+		 int index;
+		 int columnCount = 0;
+		 for(int i = 0; i < counts.length; i++)
+		 {
+			 // Calculuate the element to use since we're
+			 // filling from top to bottom, left to right
+			 index = (i / xDim) + columnCount * yDim;
+			 // Get the RGB values
+			 if(col.equals("Red"))
+			 {
+				 r = (float)counts[index] / maxCount;
+			 }
+			 else if(col.equals("Green"))
+			 {
+				 g = (float)counts[index] / maxCount;
+			 }
+			 else
+			 {
+				 b = (float)counts[index] / maxCount;
+			 }
+			 jB[index] = new JButton("");
+			 jB[index].setBackground(new Color(r, g, b));
+			 jB[index].setOpaque(true);
+			 map.add(jB[index]);
+			 columnCount++;
+			 // Reset the column count if it grows too large
+			 if(columnCount >= xDim)
+			 {
+				 columnCount = 0;
+			 }
+		 }
+		 map.setVisible(true); 
+	 }
 
 
     /**
      *  Self Organizing Maps for enhancing features
      */
-    void som(){
-
+    void som(double[][] audiofilesdata,int x, int y,int z)
+    {
+        int xVal;
+        int yVal;
+        int epochVal;
+        try
+        {
+                xVal = x;
+                yVal = y;
+                epochVal = z;
+                if(xVal <= 0 || yVal <= 0 || epochVal <= 0)
+                {
+                    throw new NumberFormatException();
+                }
+        }
+        catch(NumberFormatException nfe)
+        {
+                return;
+        }
+        long startTime = System.nanoTime();
+        SOM training = new SOM(audiofilesdata, xVal, yVal, epochVal);
+        training.train();
+		long endTime = System.nanoTime();
+        plot(xVal, yVal, training.getNodes());
     }
 
 
